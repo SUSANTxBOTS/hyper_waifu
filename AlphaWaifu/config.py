2c9a58ab9d602f0eb702e27453b6648afa274d2f
𝑷𝒓𝒊𝒎𝒆 ⚔️ 𝑯𝒚𝒑𝒆𝒓 <primehyper.official@gmail.com>
2025-10-19 01:42:55 +0530

    Update config.py

--- AlphaWaifu/config.py
+++ AlphaWaifu/config.py
@@ -2,22 +2,22 @@ import os
 
 BOT_TOKEN = os.getenv(
     "BOT_TOKEN",
-    "7442709602:AAE0Z65zpbiPnyMRAmxZISKS0m_V3soJd7w"
+    "7990481567:AAHiI1CQZDuVC0E0OkZIx9a6oP63X7uqLhI"
 )
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://xolig17286:xolig17286@cluster0.5cysryr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://Waifu_db_user:3r87a7Wd8mYDcUsy@cluster0.eogfqdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 )
 
 # space-separated IDs as default
 SUDO_USERS = [
     int(x) for x in os.getenv(
         "SUDO_USERS",
-        "7576729648 7692444709 6642049252 6239769036"
+        "7125448912"
     ).split()
 ]
 
-CHARA_CHANNEL_ID = -1002527530412
+CHARA_CHANNEL_ID = -1002531246021
 
 # ---------- RARITY MAP ---------- #
 RARITY_MAP = {
